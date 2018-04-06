dff8c52492a5e96ddac9eb8896f7cbfa874f7063
zhixuanziben <714919199@qq.com>
2018-04-06 20:18:42 +0800

    style: 删除content.js页面无用代码，并解决上次提交产生的bug

--- src/examples/views/content/Content.jsx
+++ src/examples/views/content/Content.jsx
@@ -14,19 +14,6 @@ import {
 // import TextareaMd from '../../../components/Textarea/readme.md';
 // import ToastMd from '../../../components/Toast/readme.md';
 
-<<<<<<< HEAD
-import ButtonDev from '../../components/ButtonDev.js';
-import CheckboxDev from '../../components/CheckboxDev.js';
-import FormDev from '../../components/FormDev.js';
-import InputDev from '../../components/InputDev.js';
-import ModalDev from '../../components/ModalDev.js';
-import PaginationDev from '../../components/PaginationDev.js';
-import RadioDev from '../../components/RadioDev.js';
-import SelectDev from '../../components/SelectDev.js';
-import TextareaDev from '../../components/TextareaDev.js';
-import ToastDev from '../../components/ToastDev.js';
-import TransferDev from '../../components/TransferDev.js';
-=======
 import ButtonDev from '../../components-examples/ButtonDev.js';
 import CheckboxDev from '../../components-examples/CheckboxDev.js';
 import FormDev from '../../components-examples/FormDev.js';
@@ -37,7 +24,7 @@ import RadioDev from '../../components-examples/RadioDev.js';
 import SelectDev from '../../components-examples/SelectDev.js';
 import TextareaDev from '../../components-examples/TextareaDev.js';
 import ToastDev from '../../components-examples/ToastDev.js';
->>>>>>> 1c2971980dbc9bf816e9e364beb98d7eeeccb3dc
+import TransferDev from '../../components-examples/TransferDev.js';
 import './content.css';
 const ReactMarkdown = require('react-markdown');
 // const input = '# This is a header\n\nAnd this is a paragraph'
@@ -47,36 +34,8 @@ class Content extends Component {
     super(props);
     this.state = {};
   }
-  componentDidMount() {
-  }
-  getContentComponent = () => {
-    return (
-      <div>
-        <div className="modal-header">
-            <span>提示</span>
-        </div>
-        <div className="modal-body">
-            <p>如果没有保存，数据将会丢失，确认取消吗？</p>
-        </div>
-    </div>
-    )
-  }
-  openModal = () => {
-    const myModal = modal.open({
-      "size": 'sm',
-      "template": this.getContentComponent(),
-      "onCancel": this.closeModal,
-      "onOk": this.okModal
-    });
-    // 该组件提供代码关闭弹框的手段，调用close方法即可！
-    // setTimeout(() => {
-    //   myModal.close()
-    // }, 2000);
-  }
   getCommponent = ({ match }) => {
-    console.log(match.path);
     let CommponentType;
-    console.log(SelectDev)
     switch (match.path)
     {
       case '/Button':
@@ -110,7 +69,6 @@ class Content extends Component {
         CommponentType = TransferDev;
         break;
     }
-    console.log(CommponentType)
     return <CommponentType />
   }
   render() {
